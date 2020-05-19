18628ceb90bac7e56fe8d2c3cb2f18e4ce63900e
o.fareniuk <fareniuk.olexandr@gmail.com>
2020-05-19 23:04:02 +0300

    create: firebase log-in

--- functions/index.js
+++ functions/index.js
@@ -2,15 +2,23 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const {
+    loginUser
+} = require('./APIs/users')
+
+const {
     getAllTodos,
     postOneTodo,
     deleteTodo,
     editTodo
 } = require('./APIs/todos')
 
+//Todos
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
 app.delete('/todo/:todoId', deleteTodo);
 app.put('/todo/:todoId', editTodo);
+
+//Users
+app.post('/login', loginUser);
 
 exports.api = functions.https.onRequest(app);
---
+++ functions/utils/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAV2qgTH1biik0_lnYkDWlRYhDm8UnBrB0",
+    authDomain: "todo-react-firebase-expr-1eaa7.firebaseapp.com",
+    databaseURL: "https://todo-react-firebase-expr-1eaa7.firebaseio.com",
+    projectId: "todo-react-firebase-expr-1eaa7",
+    storageBucket: "todo-react-firebase-expr-1eaa7.appspot.com",
+    messagingSenderId: "924313831936",
+    appId: "1:924313831936:web:ab8dbee3c671952625a6e5",
+    measurementId: "G-G19R1VZZ1E"
+};
